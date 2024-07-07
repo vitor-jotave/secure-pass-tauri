fcf0371b0fc00c97f9842d0242dee6a17cbfed33
João Vitor <joaovitors@live.com>
2024-07-07 16:32:56 -0300

    commit

--- src/components/Header.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Header: React.FC = () => (
-    <header className="text-4xl font-light pt-4 px-2 bg-fullblack z-20">
-        Este é o seu cofre, <span className="font-bold">Username</span>.
-    </header>
-);
-
-export default Header;
